6ad50bee1cebde904f1fd0b51c42c9bd22d58ef7
GltchyFX <gltchdreality@gmail.com>
2026-02-09 12:08:53 -0600

    basic class structure for entities

--- source/Entities/Desiderium.cpp
+++ source/Entities/Desiderium.cpp
@@ -1,1 +1,18 @@
 #include "Desiderium.h"
+#include <string>
+#include <sstream>
+
+Desiderium::Desiderium()
+:
+    Entity()
+{
+}
+
+std::string Desiderium::toString()
+{
+    std::ostringstream out;
+
+    out << Entity::toString();
+
+    return out.str();
+}
